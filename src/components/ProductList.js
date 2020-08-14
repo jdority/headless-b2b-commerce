99c7d60c4e9278cd1396da4bb2decdf17f796c42
J Dority <jdority@salesforce.com>
2020-08-14 09:59:52 -0700

    Update ProductList.js

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -46,7 +46,7 @@ export default class ProductList extends Component {
             <React.Fragment>
                 <div className="py-5">
                     <div className="container">
-                        <Title name="energy" title="products"></Title>
+                        <Title name="cirrus" title="products"></Title>
                         <ClipLoader
                             css={override}
                             size={150}
